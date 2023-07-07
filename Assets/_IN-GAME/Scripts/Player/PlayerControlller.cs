96da0e5e15e55d45671ef16bf3a4b20d7c9bc57b
akif shaikh <66055292+Akif027@users.noreply.github.com>
2023-07-08 03:19:51 +0530

    Movement Animation Done

--- Assets/_IN-GAME/Scripts/Player/PlayerControlller.cs
+++ Assets/_IN-GAME/Scripts/Player/PlayerControlller.cs
@@ -10,21 +10,41 @@ public class PlayerControlller : MonoBehaviour
 
     private Vector2 movement;
 
+    private Animator anim;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
         // Input
         movement = joystick.Direction.normalized;
-        
+     
+      //  Debug.Log(movement.x);
+
+      //-> movement Animation 
+        bool isIdle = movement.x == 0 && movement.y == 0;
+
+        if (isIdle)
+        {
+            anim.SetBool("isMoving", false);
+        }
+        else
+        {
+            anim.SetFloat("HorizontalMovement", movement.x);
+
+            anim.SetBool("isMoving", true);
+            anim.SetFloat("VerticalMovement", movement.y);
+        }
+      
     }
 
     private void FixedUpdate()
     {
         // Movement
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
     }
 }
