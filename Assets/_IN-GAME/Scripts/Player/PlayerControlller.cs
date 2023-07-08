231237f91b731f1fc2f0e73d5420482a262ede96
MAYTAST <satyam.tambakhe@gmail.com>
2023-07-09 04:54:03 +0530

    Power added +Power Manager Created + Tags added + Collection Gem Prefab Created

--- Assets/_IN-GAME/Scripts/Player/PlayerControlller.cs
+++ Assets/_IN-GAME/Scripts/Player/PlayerControlller.cs
@@ -5,7 +5,7 @@ public class PlayerControlller : MonoBehaviour
 {
     public float moveSpeed = 5f; // Adjust the movement speed as needed
     [SerializeField] private DynamicJoystick joystick;
-
+    [SerializeField] PowerManager PowerManager;
     private Rigidbody2D rb;
 
     private Vector2 movement;
@@ -34,5 +34,28 @@ public class PlayerControlller : MonoBehaviour
         // Movement
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "KillAll":
+                if (PowerManager != null)
+                {
+                    Destroy(collision.gameObject);
+                    PowerManager.KillAllEnemies();
+                }
+                break;
+            case "CollectAll":
+                if (PowerManager != null)
+                {
+                    Destroy(collision.gameObject);
+                    PowerManager.CollectAll();
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
