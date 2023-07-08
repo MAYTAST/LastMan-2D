0aa3e10b5876561d38861de15b12a4adff2fa1a7
MAYTAST <satyam.tambakhe@gmail.com>
2023-07-09 02:50:43 +0530

    Working on Ablities

--- Assets/_IN-GAME/Scripts/Managers/AbilityManager.cs
+++ Assets/_IN-GAME/Scripts/Managers/AbilityManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public enum AbilityType
 {
     RotatingBlade,
@@ -19,9 +19,13 @@ public class Ability
 public class AbilityManager : MonoBehaviour
 {
     public PlayerControlller playerControlller;
+    public GameObject BombPrefab;
     public List<Ability> abilities;
     public float SpeedUpgradeValue=0.5f;
     private Ability CurrentAblity;
+    private Transform[] AllChilds_RotatingBlade;
+    private Transform[] AllChilds_BombAblity;
+   
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.D))
@@ -71,6 +75,7 @@ public class AbilityManager : MonoBehaviour
             if (i == AblityLevel)
             {
                 CurrentAblity.LevelsGameObject[i].SetActive(true);
+                StartAblity(i);
                 break;
             }
             else
@@ -79,6 +84,28 @@ public class AbilityManager : MonoBehaviour
             }
         }
     }
+
+    private void StartAblity(int i)
+    {
+        switch (CurrentAblity.AbilityType)
+        {
+            case AbilityType.RotatingBlade:
+                StopCoroutine(nameof(RotatingBlade));
+                RotatingBladeActivated(CurrentAblity.LevelsGameObject[i]);
+                break;
+            case AbilityType.SpeedBoost:
+                break;
+            case AbilityType.Bomb:
+                StopCoroutine(nameof(SpawnBombs));
+                BombAblityActivated(CurrentAblity.LevelsGameObject[i]);
+                break;
+            case AbilityType.ForceField:
+                break;
+            default:
+                break;
+        }
+    }
+
     private void AssingCurrentAblity(AbilityType abilityType)
     {
         foreach (Ability item in abilities)
@@ -96,5 +123,49 @@ public class AbilityManager : MonoBehaviour
         {
             playerControlller.moveSpeed += SpeedUpgradeValue;
         }
+    }
+   
+    public void RotatingBladeActivated(GameObject LevelParent)
+    {
+        AllChilds_RotatingBlade = LevelParent.GetComponentsInChildren<Transform>();
+        StartCoroutine(nameof(RotatingBlade));
+    }
+    IEnumerator RotatingBlade()
+    {
+        for (int i = 1; i < AllChilds_RotatingBlade.Length; i++)//Starting from 1 because 0 index is parent
+        {
+            AllChilds_RotatingBlade[i].DOScale(0, 0f);
+            AllChilds_RotatingBlade[i].DOScale(1.75f, 1.5f);
+        }
+        yield return new WaitForSeconds(4f);
+        for (int i = 1; i < AllChilds_RotatingBlade.Length; i++)
+        {
+            AllChilds_RotatingBlade[i].DOScale(0f, 1.5f);
+        }
+        yield return new WaitForSeconds(4f);
+        StartCoroutine(nameof(RotatingBlade));
+    }
+    public void BombAblityActivated(GameObject LevelParent)
+    {
+        AllChilds_BombAblity = LevelParent.GetComponentsInChildren<Transform>();
+        StartCoroutine(nameof(SpawnBombs));
+    }
+    IEnumerator SpawnBombs()
+    {
+        for (int i = 1; i < AllChilds_BombAblity.Length; i++)//Starting from 1 because 0 index is parent
+        {
+            GameObject Bomb=Instantiate(BombPrefab,playerControlller.transform.position, Quaternion.identity, null);
+            Bomb.transform.DOMove(AllChilds_BombAblity[i].transform.position, 1f).OnComplete(() =>
+            {
+                Bomb.GetComponent<Rotation>().enabled = false;
+                Bomb.GetComponent<Animator>().enabled = true;
+                if (Bomb != null)
+                {
+                    Destroy(Bomb, 1f);
+                }
+            });
+        }
+        yield return new WaitForSeconds(3f);
+        StartCoroutine(nameof(SpawnBombs));
     }
 }
