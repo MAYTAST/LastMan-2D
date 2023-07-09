71dafaa8e5bc98133d249f6b69a0cd9d30a5772c
akif shaikh <66055292+Akif027@users.noreply.github.com>
2023-07-09 15:56:16 +0530

    attack animation problem fixed

--- Assets/_IN-GAME/Scripts/Enemy/Enemy.cs
+++ Assets/_IN-GAME/Scripts/Enemy/Enemy.cs
@@ -1,16 +1,24 @@
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    private float currentSpeed;
     private Transform playerTransform;
     private SpriteRenderer spriteRenderer;
-    public float attackRange = 2f;
-    public Animator animator;
+    private Animator animator;
 
+    public float circleRadius = 2f; // Adjust the radius of the overlapping circle
+    public LayerMask playerLayer;
+    private bool isPlayerInRange = false;
+
+    private Vector3 direction;
     private void Start()
     {
+        currentSpeed = moveSpeed;
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
     private void Awake()
     {
@@ -21,18 +29,12 @@ public class Enemy : MonoBehaviour
     private void Update()
     {
         MoveTowardsPlayer();
+        Flip();
+        DetectPlayerAndAttack();
     }
 
-    private void MoveTowardsPlayer()
+    private void Flip()
     {
-        Vector3 direction = playerTransform.position - transform.position;
-        float distanceX = direction.x;
-        direction.y += 0.5f; // Adjust the Y-component of the direction
-
-        direction.Normalize();
-
-       
-
         if (direction.x < 0f)
         {
             spriteRenderer.flipX = true;
@@ -41,17 +43,52 @@ public class Enemy : MonoBehaviour
         {
             spriteRenderer.flipX = false;
         }
+    }
 
-        if (Mathf.Abs(distanceX) <= attackRange)
+    private void DetectPlayerAndAttack()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, circleRadius, playerLayer);
+        bool isPlayerDetected = colliders.Length > 0;
+
+        if (isPlayerDetected && !isPlayerInRange)
         {
+            // Player entered the attack range
+            isPlayerInRange = true;
+            currentSpeed = 0;
             // Play attack animation
             animator.SetBool("CanAttack", true);
         }
-        else
+        else if (!isPlayerDetected && isPlayerInRange)
         {
-            transform.Translate(direction * moveSpeed * Time.deltaTime);
+            // Player exited the attack range
+            isPlayerInRange = false;
+            currentSpeed = moveSpeed;
+            // Reset the attack animation
             animator.SetBool("CanAttack", false);
         }
     }
+    private void MoveTowardsPlayer()
+    {
+        direction = playerTransform.position - transform.position;
+        float distanceX = direction.x;
+        direction.y += 0.5f; // Adjust the Y-component of the direction
 
+        direction.Normalize();
+      
+       // Debug.Log(playerTransform.position.x);
+        transform.Translate(direction * currentSpeed * Time.deltaTime);
+      
+
+       
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Visualize the overlapping circle in the editor
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, circleRadius);
+    }
+
+  
 }
